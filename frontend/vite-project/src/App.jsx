319c9f0c373c068645aaaaceedccd92aab531d78
PratishthaGautam07 <pratishthagautam2107@gmail.com>
2026-08-05 00:07:44 +0530

    intial react+tailwind+node

---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-slate-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
