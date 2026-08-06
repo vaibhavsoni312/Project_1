70071fcf63f04f07c08e37d00db09b6336c4b075
PratishthaGautam07 <pratishthagautam2107@gmail.com>
2026-08-07 01:32:20 +0530

    Added VibeCheck frontend layout

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,9 +1,11 @@
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-slate-100">
-      <h1 className="text-5xl font-bold text-blue-600">
-        Tailwind is Working 🚀
-      </h1>
+    <div className="app">
+    <div className="container">
+    <div className="sidebar"></div>
+    <div className="main"></div>
+    <div className="right-panel"></div>
+     </div>
     </div>
   );
 }
